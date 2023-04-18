12ca2c6f3cdc565bd2eb2ab55ed16cbc72f15649
Delepitora <Elmira03.ru@gmail.com>
2023-04-18 15:46:45 +0300

    Настройка проекта, добавление файлов программы и тестирования

---
+++ src/ProgramPM03.ConsoleProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramPM03.ConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
