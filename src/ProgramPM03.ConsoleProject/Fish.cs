17014725a9ba59e480b857570c29d40d2c63f62b
stu-pksp119 <Elmira03.ru@gmail.com>
2023-04-19 08:51:04 +0300

    Добавление класса Fish

---
+++ src/ProgramPM03.ConsoleProject/Fish.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramPM03.ConsoleProject
+{
+    class Fish
+    {
+        private string kind;
+        private string manufacturer;
+        private string price;
+
+        public string Kind {
+            set
+            {
+                kind = value;
+            }
+            get
+            {
+                return kind;
+            }
+        }
+        public string Manufacturer
+        {
+            set
+            {
+                manufacturer = value;
+            }
+            get
+            {
+                return manufacturer;
+            }
+        }
+        public string Price
+        {
+            set
+            {
+                price = value;
+            }
+            get
+            {
+                return price;
+            }
+        }
+
+    }
+}
